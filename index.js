de29230d135a0f22cdb2617eeaee7497545c3bd5
Takuma Hanatani <mail@potato4d.me>
2017-08-10 15:51:47 +0900

    :recycle:

--- index.js
+++ index.js
@@ -2,39 +2,29 @@ import express from 'express'
 import requests from './src/request.server'
 import bodyParser from 'body-parser'
 
-const app = new express();
+const app = new express()
 
-app.use(bodyParser());
+app.use(bodyParser())
 
-Object.entries(requests).forEach(([endpoint, validators])=>{
-  console.log(`/requests${endpoint}`)
+Object.entries(requests).map(([endpoint, validators])=>{
   app.post(`/requests${endpoint}`, (req, res) => {
     console.log(`/requests${endpoint}`)
     const errors = [];
-
     Object.entries(validators).map(([key, rule]) => {
-      // バリデータに必須フラグをつけていないので今度やる
-      // if(!(key in req)) {
-      //   errors.push('${key}がありません。')
-      // }
-
-      if(rule == 'pass') return;
-      if(!(key in req.body) || !req.body[key]) return; // 一旦省く
-      const target = req.body[key];
-
+      // TODO バリデータに必須フラグをつける
+      if(rule == 'pass') return
+      if(!(key in req.body) || !req.body[key]) return
+      const target = req.body[key]
       console.log(key, target, rule)
       console.log(target.match(rule))
-      if(target.match(rule) === null) {
-        errors.push(`${key}が不正です。マッチャ「${rule}」`);
-      }
+      (target.match(rule)||[]).length and errors.push(`${key}が不正です。マッチャ「${rule}」`)
     });
     !errors.length ? (res.json({status:'success'})) : (res.status(400) && res.json({status:'fail',errors}));
   });
 });
 
 
-var server = app.listen(3000, '0.0.0.0', () => {
-  const host = server.address().address;
-  const port = server.address().port;
-  console.log('Example app listening at http://%s:%s', host, port)
+const server = app.listen(3000, '0.0.0.0', () => {
+  const { address, port = } server.address();
+  console.log('App listening at http://%s:%s', address, port)
 });
